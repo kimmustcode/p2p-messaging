63af53822a13cf411341a9c69a79cab4044955db
Kim Manske <sangwookim1999@gmail.com>
2025-01-31 09:24:37 -0800

    added threading to server

--- client.py
+++ client.py
@@ -1,19 +1,34 @@
 import socket
+import threading
 
-SERVER_IP = "192.168.1.24"  # Replace with server's IP
+SERVER_IP = "192.168.1.24"  
 PORT = 65432
 
-def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        client_socket.connect((SERVER_IP, PORT))
-        print("Connected to the server.")
-        while True:
+# Threading needed to handles listening for messages and typing messages
+
+def messageListening():
+
+    return 
+
+def messageHandling(client_socket):
+    while True:
             message = input("Enter message (or 'exit' to quit): ")
             if message.lower() == "exit":
                 break
             client_socket.sendall(message.encode())
             data = client_socket.recv(1024)
-            print(f"Server reply: {data.decode()}")
+
+            if data: 
+                print(data.decode())
+    return
+
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((SERVER_IP, PORT))
+        print("Connected to the server.")
+        
+        messageHandling(client_socket)
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,36 +1,44 @@
 import socket
-HOST = "0.0.0.0"  # Listen on all network interfaces
-PORT = 65432      # Port to bind to
+import threading
 
+HOST = "0.0.0.0"  # listens on all network interfaces
+PORT = 65432      
+connections = []
 
 def clientConnection(client_socket, client_address):
-        with client_socket:
-            print(f"Connected by {client_address}")
-            while True:
-                try: 
-                    data = client_socket.recv(1024)
-                    if not data:
-                        print(f"{client_address} has disconnected...")
-                        break     
-                        
-                    print(f"{client_address[0]}: {data.decode()}")
-                    client_socket.sendall(b"Message received!")
+    bumper = "\n-----------------------\n"
+    with client_socket:
+        print(f"{bumper}New connection from {client_address[0]}{bumper}")
+        connections.append(client_socket)
+        while True:
+            try: 
+                data = client_socket.recv(1024)
+                if not data:
+                    print(f"{bumper}{client_address[0]} has disconnected...{bumper}")
+                    print(f"Current connections: {threading.active_count() - 1}")
+                    break     
+                
+                msg = f"{client_address[0]}: {data.decode()}"
+                print(msg)
+                for user in connections:
+                    user.sendall(msg.encode())
 
-                except:
-                    print(f"Connection lost with {client_address[0]}")
+            except:
+                print(f"{bumper}Connection lost with {client_address[0]}{bumper}")
+                connections.remove(client_socket)
+                print(f"Current connections: {threading.active_count() - 1}")
 
 def main(): 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind((HOST, PORT))
-        server_socket.listen()
 
-        print(f"Server listening on {HOST}:{PORT}...")
+        while True:
+            server_socket.listen()
+            client_socket, client_address = server_socket.accept()
 
-        client_socket, client_address = server_socket.accept()
-
-        clientConnection(client_socket, client_address)
-
-
+            thread = threading.Thread(target=clientConnection, args=(client_socket, client_address), daemon=True)
+            thread.start()
+            print(f"Current connections: {threading.active_count() - 1}")
 
 if __name__ == "__main__":
     main()
